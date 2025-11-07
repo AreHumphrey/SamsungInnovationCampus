e6937b3f4bde60f4ea7df2b3ea3b869796271590
Dina <vinnitckaya.ds@dvfu.ru>
2025-11-07 11:33:29 +1000

    Update 3.py

--- mod2/2_7/3.py
+++ mod2/2_7/3.py
@@ -5,7 +5,7 @@ alpha = 0.001
 optimizer = torch.optim.SGD([w], lr=alpha)
 
 for _ in range(500):
-    # it's critical to calculate function inside the loop:
+
     function = (w + 7).log().log().prod()
     function.backward()
     optimizer.step()
